382b2484c877cf788f0f91b049eb8a34b1ad84ad
Romanos <voulgarakisromanos@gmail.com>
2020-11-30 20:28:41 +0200

    Stat analysis script fix

--- src/statistical_analysis.m
+++ src/statistical_analysis.m
@@ -6,9 +6,9 @@ median_error(1) = median(final_error(1,:),'omitnan');
 median_error(2) = median(final_error(2,:),'omitnan');
 median_error(3) = median(final_error(3,:),'omitnan');
 
-error_cov(1) = cov(final_error(1,:),'omitrows');
-error_cov(2) = cov(final_error(2,:),'omitrows');
-error_cov(3) = cov(final_error(3,:),'omitrows');
+error_cov(1) = std(final_error(1,:),'omitnan');
+error_cov(2) = std(final_error(2,:),'omitnan');
+error_cov(3) = std(final_error(3,:),'omitnan');
 
 error_estimation(1) = median_error(1)+error_cov(1);
 error_estimation(2) = median_error(2)+error_cov(2);
