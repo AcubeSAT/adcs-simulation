9cd39f4719ff5584beb38b9550d3a7d564027725
Georgios Savvidis <giorgossav22@gmail.com>
2021-03-22 15:51:56 +0000

    Update quat_EB2OB.m

--- src/transformations/quat_EB2OB.m
+++ src/transformations/quat_EB2OB.m
@@ -1,3 +1,21 @@
+% -----------------------------------------------------------------------------
+%
+%                              procedure quat_EB2OB
+%
+%  this procedure creates a quaternion from the ECI to Orbit frame
+%
+%   inputs        :
+%   q_eci_body    - Quaternion from ECI to Body frame
+%   nodem         - Right Ascension of the Ascending Node
+%   inclm         - Inclination
+%   argpm         - Argument of Perigee
+%   mm            - Mean Anomaly
+%
+%   outputs       :
+%   q_orbit_body  - quaternion from Orbit to Body frame
+%  ----------------------------------------------------------------------------*/
+
+
 function q_orbit_body = quat_EB2OB(q_eci_body , nodem , inclm ,argpm , mm)
 
     q_orbit_eci=dcm2quat(Orbit2ECI_DCM(nodem,inclm,argpm+mm));
