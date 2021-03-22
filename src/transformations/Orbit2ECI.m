4ab767cc19a354fdea5d9b98210acf1a4aea1cff
Georgios Savvidis <giorgossav22@gmail.com>
2021-03-22 15:37:58 +0000

    Update Orbit2ECI.m

--- src/transformations/Orbit2ECI.m
+++ src/transformations/Orbit2ECI.m
@@ -1,3 +1,19 @@
+% -----------------------------------------------------------------------------
+%
+%                              procedure Orbit2ECI
+%
+%  this procedure rotates a vector from the Orbit to the ECI frame
+%
+%   inputs        :
+%   ref_vector_orbit - Vector in Orbit frame
+%   nodeo           - Right Ascension of the Ascending Node
+%   incl          - Inclination
+%   argp_mo       - Addition of Argument of Perigee with Mean Anomaly
+%
+%   outputs       :
+%   ref_vector_eci - Vector in ECI frame
+%  ----------------------------------------------------------------------------*/
+
 function ref_vector_eci = Orbit2ECI(ref_vector_orbit,nodeo,incl,argp_mo)
 R=zeros(3,3);
 R(1,1) = -sin(argp_mo) * sin(nodeo) * cos(incl) + cos(argp_mo) * cos(nodeo);
@@ -12,6 +28,7 @@ R(3,1) = sin(incl) * sin(nodeo);
 R(3,2) = -sin(incl) * cos(nodeo);
 R(3,3) = cos(incl);
 
+%% Rotation of the initial Orbit frame to coincide it with the Body frame
 R_star = [-R(1,:); R(3,:); R(2,:)];
 
 ref_vector_eci=R_star'*ref_vector_orbit;
