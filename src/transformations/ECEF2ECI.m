6ad81978b425be7ada6736e36e043876dcd46432
Georgios Savvidis <giorgossav22@gmail.com>
2021-03-22 14:51:32 +0000

    Update ECEF2ECI.m

--- src/transformations/ECEF2ECI.m
+++ src/transformations/ECEF2ECI.m
@@ -1,13 +1,29 @@
+% -----------------------------------------------------------------------------
+%
+%                              procedure ECEF2ECI
+%
+%  this procedure rotates a vector from the ECEF to the ECI frame
+%
+%   inputs        :
+%   vec_ecef      - Vector in ECEF frame
+%   v_vec_ecef    - Velocity vector in ECEF frame
+%   gst           - Time in GST format
+%   vel           - Velocity transfromation constant (1 = transform)
+%
+%   outputs       :
+%   vec_eci       - Vector in ECI frame
+%   v_vec_eci     - Velocity vector in ECI frame
+%  ----------------------------------------------------------------------------*/
+
+
 function [vec_eci, v_vec_eci]= ECEF2ECI(vec_ecef,v_vec_ecef,gst,vel)
-%Now rotate the coordinates
+%% ----- Vector -----
 CGAST = cos(-gst); SGAST = sin(-gst);
 R=[CGAST SGAST 0; -SGAST CGAST 0; 0 0 1];
-% temp=R(2,:);
-% R(2,:)=R(1,:);
-% R(1,:)=temp;
 vec_eci=R*vec_ecef;
+
+%% ----- Velocity Vector -----
 if vel == 1 
-    %Apply rotation to convert velocity vector from ECI to ECEF coordinates
     OMEGAE = 7.29211586D-5;  %Earth rotation rate in rad/s
     temp=(CGAST^(2)-SGAST)
     v_vec_eci(1)=(v_vec_ecef(1)*CGAST-v_vec_ecef(2)+vel*OMEGAE*vec_eci(1)-CGAST*vel*OMEGAE*vec_eci(2))/temp;
@@ -16,5 +32,6 @@ if vel == 1
 else
     v_vec_eci=zeros(3,1);
 end
+
 v_vec_eci=R';
 end
