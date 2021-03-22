c6ea4cc1b1cf578dfcb9c79e328febc9812c43c6
Georgios Savvidis <giorgossav22@gmail.com>
2021-03-22 14:54:49 +0000

    Update NED2ECEF.m

--- src/transformations/NED2ECEF.m
+++ src/transformations/NED2ECEF.m
@@ -1,3 +1,17 @@
+% -----------------------------------------------------------------------------
+%
+%                              procedure NED2ECEF
+%
+%  this procedure rotates a vector from the NED to the ECEF frame
+%
+%   inputs        :
+%   ref_vec_ned   - Vector in NED frame
+%   lat           - Latitude
+%   long          - Longitude
+%
+%   outputs       :
+%   ref_vector_ecef - Vector in ECEF frame
+%  ----------------------------------------------------------------------------*/
 function ref_vector_ecef = NED2ECEF(ref_vec_ned,lat,long)
 for n=1:size(ref_vec_ned,2)
     c1=[-sin(lat(n))*cos(long(n));-sin(lat(n))*sin(long(n));cos(lat(n))];
