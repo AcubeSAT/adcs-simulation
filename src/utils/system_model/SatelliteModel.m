ec646e2c73c92bcb31b1005ef8e0a5a29771cb88
Valia Pappa <valia.pp8@gmail.com>
2021-03-22 23:33:48 +0200

    System model update

--- src/utils/system_model/SatelliteModel.m
+++ src/utils/system_model/SatelliteModel.m
@@ -1,5 +1,16 @@
-%% Satellite model
-%
+% ======================================================================== %
+%     Class that implements the space environment used by the satellite
+% 
+%     Properties:
+%         dt                 - Simulation time step
+%         I                  - Inertia
+%         magn_ref           - Magnetic field
+%         torq               - Applied torque
+%         gyro               - Gyro measurements
+%         sun_ref            - Sun position
+%         eclipse            - Existence or not of eclipse conditions
+%         rw_ang_momentum    - RW angular momentum
+% ======================================================================== %
 
 classdef SatelliteModel
 
@@ -21,23 +32,15 @@ classdef SatelliteModel
         % @param[out] x_next: state at time k+1.
         %
         function x_next = stateTransFun(this, x, cookie)
-
-            % if (nargin < 3), cookie = []; end
             
-            % Local Torque parameter
             this.torq = cookie.torq;
             this.gyro = cookie.gyro;
-            
-            % Bias propagation bdot = 0
+
             bias = x(5:7);
-            
-            % Local quaternion and angular velocity parameters
             Q = x(1:4);
-            vRot = this.gyro - bias; % correction with bias
+            vRot = this.gyro - bias;
                                 
-            % Calculating quaternion dot based on kinematics model
             Q_next = quatProd( Q, quatExp(vRot*this.dt));
-            
             x_next = [Q_next; bias];
 
         end
@@ -49,37 +52,26 @@ classdef SatelliteModel
         % @param[out] y: measurements at time k.
         %
         function y = msrFun(this, x, cookie)
-
-            % if (nargin < 3), cookie = []; end
             
-            % Initializing all needed parameters
-            this.magn_ref = cookie.magn_ref; %magnetic field
-            this.sun_ref = cookie.sun_ref; %sun position
-            this.eclipse = cookie.eclipse; %eclipse calulation
+            this.magn_ref = cookie.magn_ref; 
+            this.sun_ref = cookie.sun_ref;
+            this.eclipse = cookie.eclipse;
             this.magn_ref=this.magn_ref/norm(this.magn_ref);
             this.sun_ref=this.sun_ref/norm(this.sun_ref);
             this.gyro = cookie.gyro;
             
-            Q = x(1:4); %quaternion
+            Q = x(1:4);
             
-            y = zeros(6,1); %measurements
-             
-            y(4:6) = this.gyro - x(5:7); %      Should I update?   We
-            %don't propagate angular velocity
-%             y(4:6) = this.gyro;   
+            y = zeros(6,1);
+            y(4:6) = this.gyro - x(5:7);
             
-            % Calculating magnetometer measurements
             y1 = quatProd( quatInv(Q), quatProd([0; this.magn_ref], Q) );
             
-            y(1:3) = y1(2:4); % quat2dcm(Q')*this.magn_ref;
-            y(1:3) = y(1:3)/norm(y(1:3));
-            
+            y(1:3) = y1(2:4); 
+            y(1:3) = y(1:3)/norm(y(1:3));          
 
-            %If we have eclipse, we dont have a sun estimate
             if this.eclipse == 0
-                %y2 = quatProd( quatInv(Q), quatProd([0; this.sun_ref], Q) );
                 y(7:9) = css_compensation(this.sun_ref,Q,cookie.xsat_eci,cookie.albedo,cookie.lambda);
-                %y(7:9) = y2(2:4);                
             else
                 y(7:9) = zeros(3,1);
             end
@@ -97,7 +89,7 @@ classdef SatelliteModel
            z_hat = this.msrFun(x, cookie);
             
            H = [skew(z_hat(1:3)) zeros(3,3);...
-                skew(z_hat(4:6)) zeros(3,3);skew(z_hat(7:9)) zeros(3,3)]; % Crassidis MEKF H_k 
+                skew(z_hat(4:6)) zeros(3,3);skew(z_hat(7:9)) zeros(3,3)];
         end
 
 
@@ -117,8 +109,8 @@ classdef SatelliteModel
 
     properties (Access = protected)
         
-        dt % simulation time step
-        I % inertia
+        dt
+        I
         magn_ref
         torq
         gyro
