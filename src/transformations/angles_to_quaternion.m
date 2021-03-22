183dd37401d850041627865a0aa2c624d5206a8f
Georgios Savvidis <giorgossav22@gmail.com>
2021-03-22 15:47:16 +0000

    Update angles_to_quaternion.m

--- src/transformations/angles_to_quaternion.m
+++ src/transformations/angles_to_quaternion.m
@@ -1,7 +1,22 @@
+% -----------------------------------------------------------------------------
+%
+%                              procedure angles_to_quaternion
+%
+%  this procedure creates a quaternion from the euler angles
+%
+%   inputs        :
+%   x             - Euler Angles in X axis
+%   y             - Euler Angles in Y axis
+%   z             - Euler Angles in Z axis
+%
+%   outputs       :
+%   quaternion    - quaternion from the Euler angles
+%  ----------------------------------------------------------------------------*/
+
+
 function [quaternion] = angles_to_quaternion(x,y,z)
-    %x,y,z angles in each axis, in rads
     Rotation_x = [1 0 0; 0 cos(x) -sin(x); 0 sin(x) cos(x)];
-    Rotation_y = [cos(y) 0 sin(y); 0 1 0; -sin(y) 0 cos(y)]; %sign in sin according to Wikipedia
+    Rotation_y = [cos(y) 0 sin(y); 0 1 0; -sin(y) 0 cos(y)];
     Rotation_z = [cos(z) -sin(z) 0; sin(z) cos(z) 0; 0 0 1];
     Rotation_final = Rotation_z*Rotation_y*Rotation_x;
     quaternion = d2q(Rotation_final);
