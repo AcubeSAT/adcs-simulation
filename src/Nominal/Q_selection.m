5102307f7094ba050b17201c1e17e568b3af9e30
Valia Pappa <valia.pp8@gmail.com>
2021-05-04 18:01:06 +0300

    Gain tuning for 11PM

--- src/Nominal/Q_selection.m
+++ src/Nominal/Q_selection.m
@@ -3,13 +3,13 @@ function Q_selection(Eclipse,Q_no_eclipse,R_no_eclipse,mekf,Q_eclipse_load)
     if (Eclipse)
         % Variances
         %Q = Q_eclipse_load; % Variance of the process noise w[cycle_index]
-        Q_eclipse = 8.66e-04*diag([7.68 7.68 7.68 1 1 1]);    
-        R_eclipse = [2; 2; 2; 12; 12; 12].*eye(6,6);
-%         number_of_measurements = 6;
-        % R Variances used in MEKF
-%         R_hat_coeff=[1e-8;1e-8;1e-8;1;1;1];
-%         R_hat_coeff=10000*[.5e-3;.5e-3;.5e-3;4e-3;4e-3;4e-3;1e-3;1e-3;1e-3];
-%         R_hat = R_hat_coeff.*eye(number_of_measurements,number_of_measurements);
+        
+        Q_eclipse = 8.66e-04*diag([7.68 7.68 7.68 1 1 1]);      % 6 PM
+        R_eclipse = [2; 2; 2; 12; 12; 12].*eye(6,6);            % 6 PM   
+
+%         Q_eclipse = 1e-4*diag([1 1 1 1e-3 1e-3 1e-3]);          % 11 PM
+%         R_eclipse =[1e-8;1e-8;1e-8;1;1;1].*eye(6, 6);           % 11 PM
+        
         mekf.setProcessNoiseCov(Q_eclipse); %Q variance matrix
         mekf.setMeasureNoiseCov(R_eclipse); %R variance matrix
     else
