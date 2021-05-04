cb26177518055fd5db3e4cf394faabeedf08c404
Manolis Mylonas <mylowade98@gmail.com>
2021-05-04 15:40:02 +0300

    Gains for 6 PM, Q,R, Kd, Kp

--- src/Nominal/PD.m
+++ src/Nominal/PD.m
@@ -64,9 +64,10 @@ function  [torque, T_rw, T_magnetic_effective, V_rw, I_rw, P_thermal_rw, AngVel_
     global Jw;
     global Max_RW_torq;
 
-%     if Eclipse
-%         Kd_gain = 10.* Kd_gain;
-%     end
+    if (Eclipse)
+        Kp_gain = 0.8*Kp_gain;
+        Kd_gain = 10.* Kd_gain;
+    end
     
     q_w_b_io = quatProd(quatconj(q_orbit_body'), quatProd([0;w_o_io], q_orbit_body));
     w_b_io = q_w_b_io(2:4);
--- src/Nominal/Q_selection.m
+++ src/Nominal/Q_selection.m
@@ -3,14 +3,15 @@ function Q_selection(Eclipse,Q_no_eclipse,R_no_eclipse,mekf,Q_eclipse_load)
     if (Eclipse)
         % Variances
         %Q = Q_eclipse_load; % Variance of the process noise w[cycle_index]
-        Q = 1e-4*diag([1 1 1 1e-3 1e-3 1e-3]);
-        number_of_measurements = 6;
+        Q_eclipse = 8.66e-04*diag([7.68 7.68 7.68 1 1 1]);    
+        R_eclipse = [2; 2; 2; 12; 12; 12].*eye(6,6);
+%         number_of_measurements = 6;
         % R Variances used in MEKF
-        R_hat_coeff=[1e-8;1e-8;1e-8;1;1;1];
+%         R_hat_coeff=[1e-8;1e-8;1e-8;1;1;1];
 %         R_hat_coeff=10000*[.5e-3;.5e-3;.5e-3;4e-3;4e-3;4e-3;1e-3;1e-3;1e-3];
-        R_hat = R_hat_coeff.*eye(number_of_measurements,number_of_measurements);
-        mekf.setProcessNoiseCov(Q); %Q variance matrix
-        mekf.setMeasureNoiseCov(R_hat); %R variance matrix
+%         R_hat = R_hat_coeff.*eye(number_of_measurements,number_of_measurements);
+        mekf.setProcessNoiseCov(Q_eclipse); %Q variance matrix
+        mekf.setMeasureNoiseCov(R_eclipse); %R variance matrix
     else
         mekf.setProcessNoiseCov(Q_no_eclipse); %Q variance matrix
         mekf.setMeasureNoiseCov(R_no_eclipse); %R variance matrix
