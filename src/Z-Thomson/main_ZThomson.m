9bc3a32f2dd58154f4de3f38462426746a8ae954
catherinepapad <katerinapapado.01@gmail.com>
2024-12-27 19:35:10 +0200

    First attempt

--- src/Z-Thomson/main_ZThomson.m
+++ src/Z-Thomson/main_ZThomson.m
@@ -1,7 +1,3 @@
-
-
-
-
 % ========================================================================
 %   Main function for Z-Thomson Mode simulation.
 % ========================================================================
@@ -12,10 +8,86 @@ clear variables;
 
 %% Initialize Parameters Script
 Const = constants();
-setParams_ZThomson();
+Param = setParams_ZThomson(Const.I);
+
+dt_model = Param.dt_model;
+orbits = Param.orbits;
+x0 = Param.x0;
+x0_hat = Param.x0_hat;
+real_model = Param.real_model;
+model = Param.model;
+
+albedo = Param.albedo;
+albedo_inaccurate = Param.albedo_inaccurate;
+setDisturbances = Param.setDisturbances;
+
+eclipse = Param.eclipse;
+mag_field_eci = Param.mag_field_eci;
+mag_field_orbit = Param.mag_field_orbit;
+sun_pos_eci = Param.sun_pos_eci;
+sun_pos_orbit = Param.sun_pos_orbit;
+xsat_eci = Param.xsat_eci;
+argpm = Param.argpm;
+nodem = Param.nodem;
+inclm = Param.inclm;
+mm = Param.mm;
+init_bias = Param.init_bias;
+Q = Param.Q;
+R = Param.R;
+R_hat = Param.R_hat;
+sigma_u = Param.sigma_u;
+sigma_v = Param.sigma_v;
+P0 = Param.P0;
+number_of_measurements = Param.number_of_measurements;
+use_analytic_jacob = Param.use_analytic_jacob;
+total_limit = Param.total_limit;
+cycle_duration = Param.cycle_duration;
+Total_simulation_time = Param.tst;
+R_old = Param.R_old;
+satrec = Param.satrec;
+Kd = Param.Kd;
+Ks = Param.Ks;
+w_ref = Param.w_ref;
+
+%% Initialize Global Parameters
+global R_coils; R_coils=Const.R_coils;
+global N_coils; N_coils=Const.N_coils;
+global A_coils; A_coils=Const.A_coils;
+global mt; mt = Const.mt;
+global Jw; Jw=Const.Jw;
+global Km; Km=Const.Km;
+global Kv; Kv=Const.Kv;
+global A; A=Const.A;
+global b_friction; b_friction=Const.b_friction;
+global c_friction; c_friction=Const.c_friction;
+global Rb; Rb=Const.Rb;
+global flag; flag = 0;
+global Max_RW_torq; Max_RW_torq=Const.rw_max_torque;
+
+
+%% Construct the MEKF
+Q_struct = load('idealQ.mat', 'IDEALQ');
+Q_eclipse_load = Q_struct.IDEALQ;
+n_params = length(x0_hat);                                                          % Init number of parameters
+mekf = MEKF(n_params, number_of_measurements, @model.stateTransFun, @model.msrFun); % Init EKF
+mekf.global_state = x0_hat;                                                         % Init state estimation
+mekf.P = P0;                                                                        % Init Covariance matrix
+mekf.setProcessNoiseCov(Q);                                                         % Q variance matrix
+mekf.setMeasureNoiseCov(R_hat);                                                     % R variance matrix
+mekf.setFadingMemoryCoeff(1.00);                                                    % This parameter defined the "memory" of the filter, 1 meaning regular
+mekf.setPartDerivStep(0.001);                                                       % Arithmetical jacobian step
+if (use_analytic_jacob)                                                             % If analytical jacobian is used, define the functions
+    mekf.setStateTransFunJacob(@model.stateTransFunJacob);
+    mekf.setMsrFunJacob(@model.msrFunJacob);
+end
+
+%% Initialize simulation parameters
+
 Time = 0:cycle_duration:Total_simulation_time;
-x_data = zeros(7, length(Time)); % Real state
-x = x0;
+x_real = zeros(7, length(Time)); % Real state
+q_ob_data = zeros(4,length(Time));
+x = x0(1:7);
+x_real(:,1) = x0(1:7);
 t = 0;
 B_body = zeros(3, length(Time));                    % Earth magnetic field satellite in body frame
 w_b_ob = zeros(3, length(Time));                    % Angular velocity from orbit to body frame expressed in body frame
@@ -34,10 +106,129 @@ theta_deg_arr_y = zeros(1,length(Time));
 theta_deg_arr_z = zeros(1,length(Time));
 q_orbit_body_data = zeros(4,length(Time));
 w=zeros(3,length(Time));
-global R_coils; R_coils = Const.R_coils;
-global N_coils; N_coils = Const.N_coils;
-global A_coils; A_coils = Const.A_coils;
-global mt; mt = Const.mt;
+
+
+tau_mtq = zeros(3, length(Time));           % Torques produced by MTQs
+M_data = zeros(3, length(Time));
+tau_dist = zeros(3, length(Time));
+lambda=1;
+tau_ad = zeros(3, length(Time));
+tau_rm = zeros(3, length(Time));
+tau_g = zeros(3, length(Time));
+tau_sp = zeros(3, length(Time));
+plotter_step = 1;
+reps = length(Time);
+
+x_hat_data = zeros(7,length(Time));
+bias_data = zeros(3,length(Time));
+gyro_noise_data = zeros(3,length(Time));
+Bbody_data = zeros(3,length(Time));
+
+bias_wahba_loops = 2;
+N_Timesteps = 10;
+for cycle_index = 1:bias_wahba_loops
+    current_timestep = (cycle_index - 1) * N_Timesteps + 1;
+    if eclipse(current_timestep) == 0
+
+        %% Measurements
+
+        y_real = real_model.msrFun(x, msrCookieFinal(mag_field_eci(:, current_timestep), ...
+            sun_pos_eci(:, current_timestep), eclipse(current_timestep), [0; 0; 0]));
+        y_noise = y_real + sqrt(R) * randn(size(y_real));
+        [gyro_noise, real_bias] = gyro_noise_func(real_bias, dt, sigma_u, sigma_v);
+
+
+        y_noise(4:6) = y_real(4:6) + gyro_noise;
+        % sign=randi([0 1]);
+        % if sign==0
+        %     sign=-1;
+        % end
+        y_noise(7:9) = css_noise(sun_pos_eci(:, current_timestep), x(1:4), xsat_eci(:, current_timestep), albedo(:, current_timestep), lambda);
+
+        if eclipse((cycle_index - 1)*N_Timesteps+1) ~= 0
+            y_noise(7:9) = zeros(3, 1);
+        else
+            y_noise(7:9) = y_noise(7:9) / norm(y_noise(7:9));
+        end
+        y_noise(1:3) = y_noise(1:3) / norm(y_noise(1:3));
+
+        %% Wahba
+
+        [q_wahba,~]=wahba(y_noise(7:9),y_noise(1:3),sun_pos_eci(:,current_timestep),mag_field_eci(:,current_timestep)*10^(-9));
+        mekf.global_state(1:4) = q_wahba';
+
+
+        %% Bias timer
+
+        bias_init_counter = bias_init_counter + 1;
+        % We add zeros for every timestep before the initialization is finished
+        if (bias_init_counter < bias_wahba_loops + 1)
+            quat_pos(:,bias_init_counter) = q_wahba;
+            %Set measurements to 0 until bias has initialized
+
+            for i=1:10
+                %% Current SGP4 matrices values
+
+                Nodem = nodem(1,current_timestep);
+                Inclm = inclm(1,current_timestep);
+                Argpm = argpm(1,current_timestep);
+                Mm = mm(1,current_timestep);
+                Sun_pos_orbit = sun_pos_orbit(:,current_timestep);
+                Mag_field_orbit = mag_field_orbit(:,current_timestep)*10^(-9);
+
+                %% Propagate the system
+
+                current_timestep = (cycle_index-1)*N_Timesteps+i;
+
+                q_ob = quat_EB2OB(x(1:4),Nodem,Inclm,Argpm,Mm);
+                [T_dist, ~,~,~,~,~,~] = disturbances_pd(q_ob, Sun_pos_orbit,Mag_field_orbit, disturbancesEnabled);
+
+                torq = T_dist;
+
+                x = real_model.stateTransFun(x, stateTransCookieFinalNominal(torq,rw_ang_momentum,[0;0;0]));
+                x_real(:,current_timestep)=x;
+                t = t + dt;
+
+                %% Matrices update
+
+                x_hat_data(:,current_timestep) =  zeros(7,1);
+                q_ob_data(:,current_timestep) = quat_EB2OB(x(1:4), nodem(1,current_timestep),...
+                inclm(1,current_timestep),argpm(1,current_timestep),mm(1,current_timestep) );
+                bias_data(:,current_timestep) = real_bias;
+                gyro_noise_data(:,current_timestep) = gyro_noise;
+            end
+            continue
+        end
+        %% Bias calculation
+
+        dqdt = zeros(4,bias_wahba_loops-1);
+        for i=2:bias_wahba_loops
+            dqdt(:,i-1) = quat_pos(:,i)-quat_pos(:,i-1);
+        end
+
+        estimated_rate = zeros(3,bias_wahba_loops-1);
+
+        % The angular rate is calculated using: angular_rate = 2 * dq/dt * q^-1
+        for i=1:bias_wahba_loops-1
+            temp = 2*quatProd(quatconj(quat_pos(:,i)'),dqdt(:,i));
+            estimated_rate(:,i) = temp(2:4);
+        end
+
+        real_rate = y_noise(4:6);
+
+        mean_omega = [0;0;0];        % Time averaging for noise reduction
+
+        for i=1:3
+            mean_omega(i) = mean(estimated_rate(i,:));
+        end
+
+        initial_bias_estimate = real_rate-mean_omega;
+        mekf.global_state(5:7) = initial_bias_estimate; %Initialize angular velocity equal to gyroscope measurement
+
+    end
+end
+
+
 
 for current_cycle = 1:length(Time) %Main loop
 
@@ -48,7 +239,7 @@ for current_cycle = 1:length(Time) %Main loop
     q_orbit_body_data(:,current_cycle) = q_orbit_body;
     R_OB = quat2dcm(q_orbit_body'); % Calculating the transformation matrix from orbit to body frame
     B_body(:, current_cycle) = R_OB * mag_field_orbit(:, current_cycle) * 10^(-9);
-    B_body(:, current_cycle) = B_body(:, current_cycle) + sqrt(R) * randn(size(B_body(:, current_cycle))); % Adding white noise to magnetometer measurements
+    B_body(:, current_cycle) = B_body(:, current_cycle) + sqrt(R_old) * randn(size(B_body(:, current_cycle))); % Adding white noise to magnetometer measurements
     R_BO = R_OB';
     w_b_io = R_OB(:, 3) * Const.w_o;
     w_b_ob(:, current_cycle) = x(5:7) - w_b_io; % Calculating angular rate of satellite relative to ECI frame
@@ -69,7 +260,7 @@ for current_cycle = 1:length(Time) %Main loop
     q_orbit_body = quatProd(q_orbit_eci, q_eci_body);
     R_OB = quat2dcm(q_orbit_body'); % Calculating the transformation matrix from orbit to body frame
     B_body_2 = R_OB * mag_field_orbit2 * 10^(-9);
-    B_body_2 = B_body_2 + sqrt(R) * randn(size(B_body(:, current_cycle))); % Second measurment from magnetometer
+    B_body_2 = B_body_2 + sqrt(R_old) * randn(size(B_body(:, current_cycle))); % Second measurment from magnetometer
     R_EB=quat2dcm(q_eci_body');
     %% Bdot calculation
     Bx=acos(B_body(1,current_cycle)/norm(B_body(:,current_cycle)));
@@ -140,11 +331,11 @@ for current_cycle = 1:length(Time) %Main loop
 
          M(3,1)= Kd * Bdot_body_z(current_cycle);
          if (abs(B_body(2,current_cycle))>abs(B_body(1,current_cycle)))
-            M(1,1)= Ks*(abs(w_b_ib(3,current_cycle))-w_ref)*sign(B_body(2,current_cycle));
+            M(1,1)= -Ks*(abs(w_b_ib(3,current_cycle))-w_ref)*sign(B_body(2,current_cycle));
             M(2,1)= 0;
          elseif (abs(B_body(2,current_cycle))<abs(B_body(1,current_cycle)))
             M(1,1)= 0;
-            M(2,1)=-Ks*(abs(w_b_ib(3,current_cycle))-w_ref)*sign(B_body(1,current_cycle));
+            M(2,1)=Ks*(abs(w_b_ib(3,current_cycle))-w_ref)*sign(B_body(1,current_cycle));
          end  
 
 
--- src/Z-Thomson/setParams_ZThomson.m
+++ src/Z-Thomson/setParams_ZThomson.m
@@ -2,24 +2,38 @@
 %   Script for parameters definition in Z-Thomson Mode
 % ========================================================================
 
+function Param = setParams_ZThomson(I)
+
 %% ======= Satellite ========
 cycle_duration = 1;                                         % Timestep for Controller & Orbit Propagator
+dt = 0.1;
 dt_model = .001;                                            % Timestep for Model
-orbits = 4;
+orbits = 1;
 orbitPeriod = 5545;
+tf = orbits * orbitPeriod;  %Total Simulation Seconds
 Total_simulation_time = orbits * orbitPeriod;               % Total Simulation Seconds
 Q0 = [0.3757; 0.5983; -0.2364; -0.6671];                    % Initial Quaternion in ECI frame
 Q0 = Q0 / norm(Q0);                                         % Normalised Quaternion
 vRot0 = [0.05 ; 0.05 ; 0.016];                          % Initial Angular Velocities from Body to ECI frame expressed in Body.
 x0 = [Q0; vRot0];
-real_model = real_SatelliteModel_Bdot(dt_model, Const.I);   % Initialize Satellite Model Class
-w_ref1 = 0.087;
-w_ref2 = 0.17;                          % Desired angular velocity [rad/s]
+%real_model = real_SatelliteModel_Bdot(dt_model, I);   % Initialize Satellite Model Class                          % Desired angular velocity [rad/s]
 w_ref = 0.087;
 
 %% ======= Orbit Propagation ========
 satrec = orbit_init();
-[xsat_ecf, vsat_ecf, xsat_eci, vsat_eci, sat_llh, eclipse, mag_field_ned, mag_field_eci, mag_field_ecef, mag_field_orbit, sun_pos_ned, sun_pos_eci, sun_pos_ecef, sun_pos_orbit, satrec, argpm, nodem, inclm, mm, xnode, xinc] = orbit_sgp4(satrec, cycle_duration, cycle_duration+Total_simulation_time);
+[xsat_ecf, vsat_ecf, xsat_eci, vsat_eci, sat_llh, eclipse, mag_field_ned, mag_field_eci, mag_field_ecef, mag_field_orbit, sun_pos_ned, sun_pos_eci, sun_pos_ecef, sun_pos_orbit, satrec, argpm, nodem, inclm, mm, xnode, xinc] = orbit_sgp4_offset(satrec,dt,tf+dt,1000);
+
+%% ============== Calculations ====================================
+init_bias = [.01; 0.15; -.08];      % bias initialization
+vRot0 = [0; 0; 0];                  %Initial Angular Velocities from Body to ECI frame expressed in Body.
+x0 = [Q0; vRot0];                   %Initial state consists of [Quaternion;Angular Velocity]
+Q0_hat = [.6; .1; -.7; .01];        %Random Initial State Estimation
+Q0_hat = Q0_hat / norm(Q0_hat);
+bias_hat = [0; 0; 0];
+x0_hat = [Q0_hat; bias_hat];
+model = SatelliteModel(dt, I);      %Initialize Satellite Model Class
+real_model = real_SatelliteModel_Bdot(dt_model, I);
+N_Timesteps = 10;
 
 %% ======= Simulation Constants ========
 setDisturbances = "total";  % Set which disturbances you want to activate: tau_g, tau_ad, tau_sp, tau_rm, total, zero
@@ -29,13 +43,95 @@ plotter_step = 10;
 Kd = 100;
 Ks = 1;
 
+sigma_u = 3.4434e-4;
+sigma_v = 0.0004;
+
+%% ======= Albedo ========
+%albedo = load("sso_albedo.mat");  % Choose albedo depending on orbit
+%albedo = load("iss_albedo.mat");
+base_albedo = load("SSO_500_6PM_1_Orbit_051231.mat");
+base_albedo = base_albedo.new;
+base_albedo_inaccurate = load("SSO_500_6PM_1_Orbit_050101");
+base_albedo_inaccurate = base_albedo_inaccurate.new;
+albedo = base_albedo;
+albedo_inaccurate = base_albedo_inaccurate;
+if orbits > 1
+    for i = 1:orbits - 1
+        albedo = [albedo, base_albedo];
+        albedo_inaccurate = [albedo_inaccurate, base_albedo_inaccurate];
+    end
+end
+%albedo_inaccurate = load("sso_albedo.mat");
+
+
 %% ======= Sensors ========
 n_msr = 3; % Measurements length
-R_coeff = [9e-16; 9e-16; 9e-16];
-R = R_coeff .* eye(n_msr, n_msr); % Variance of the measurement noise v[k]
+R_coeff_old = [9e-16; 9e-16; 9e-16];
+R_old = R_coeff_old .* eye(n_msr, n_msr); % Variance of the measurement noise v[k]
 
+%% ======= Kalman filter params ========
+Q = 1e-4 * diag([1, 1, 1, 1e-3, 1e-3, 1e-3]);
+
+%MGN noise 1e-3 (norm) | GYRO noise 1.57e-2| SUN noise 8.7e-3(norm)
+% R_coeff=[1e-6;1e-6;1e-6;5e-5;5e-5;5e-5;1.2e-5;1.2e-5;1.2e-5];
+R_coeff = [1.83e-6; 1.83e-6; 1.83e-6; 0; 0; 0; 0; 0; 0];
+number_of_measurements = 9; %Measurements length
+R = R_coeff .* eye(number_of_measurements, number_of_measurements); % Variance of the measurement noise v[k]
+
+% R Variances used in MEKF
+% R_hat_coeff=[1e-3;1e-3;1e-3;8e-3;8e-3;8e-3;5e-3;5e-3;5e-3];
+R_hat_coeff = [.5e-3; .5e-3; .5e-3; 1e-3; 1e-3; 1e-3];
+R_hat = R_hat_coeff .* eye(6, 6);
+% Initialize Covariance matrix
+n_dim_error = 6; % error state length
+P0 = 1 * eye(n_dim_error, n_dim_error);
+use_analytic_jacob = true;
 
 %% Parameters for threshold limits
 
 total_limit = 520;
 exceptions_limit = 30;
+
+%%  Passing the values of the parameters in a struct.
+Param.N_Timesteps = N_Timesteps;
+Param.tf = tf;
+Param.w_ref = w_ref;
+Param.Kd = Kd;
+Param.Ks = Ks;
+Param.satrec = satrec;
+Param.R_old = R_old;
+Param.tst = Total_simulation_time;
+Param.cycle_duration = cycle_duration;
+Param.dt = dt;
+Param.orbits = orbits;
+Param.x0 = x0;
+Param.x0_hat = x0_hat;
+Param.real_model = real_model;
+Param.model = model;
+Param.albedo = albedo;
+Param.albedo_inaccurate = albedo_inaccurate;
+Param.setDisturbances = setDisturbances;
+Param.eclipse = eclipse;
+Param.mag_field_eci = mag_field_eci;
+Param.mag_field_orbit = mag_field_orbit;
+Param.sun_pos_eci = sun_pos_eci;
+Param.sun_pos_orbit = sun_pos_orbit;
+Param.argpm = argpm;
+Param.nodem = nodem;
+Param.inclm = inclm;
+Param.mm = mm;
+Param.init_bias = init_bias;
+Param.Q = Q;
+Param.R_coeff = R_coeff;
+Param.R = R;
+Param.R_hat = R_hat;
+Param.sigma_u = sigma_u;
+Param.sigma_v = sigma_v;
+Param.P0 = P0;
+Param.number_of_measurements = number_of_measurements;
+Param.use_analytic_jacob = use_analytic_jacob;
+Param.xsat_eci = xsat_eci;
+Param.total_limit= total_limit;
+
+
+end
