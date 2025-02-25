62e73af99afa0bb32aa471d52fd0a5ab0ea8beef
Alexandra Tsela <atsela@ece.auth.gr>
2025-02-25 14:03:53 +0000

    indent lines one tab in

--- src/Z-Thomson/Control_Thomson.m
+++ src/Z-Thomson/Control_Thomson.m
@@ -25,33 +25,33 @@
 function [Bdot_body,torq,V_mtq, I_mtq, P_thermal_mtq,M] = ...
         Control_Thomson(w_b_ib, B_body_2,B_body, mtq_max, Kd, Ks, w_ref)
 
-Bx=acos(B_body(1)/norm(B_body));
-Bx2=acos(B_body_2(1)/norm(B_body_2));
+    Bx=acos(B_body(1)/norm(B_body));
+    Bx2=acos(B_body_2(1)/norm(B_body_2));
 
-By=acos(B_body(2)/norm(B_body));
-By2=acos(B_body_2(2)/norm(B_body_2));
+    By=acos(B_body(2)/norm(B_body));
+    By2=acos(B_body_2(2)/norm(B_body_2));
 
-Bz=acos(B_body(3)/norm(B_body));
-Bz2=acos(B_body_2(3)/norm(B_body_2));
+    Bz=acos(B_body(3)/norm(B_body));
+    Bz2=acos(B_body_2(3)/norm(B_body_2));
 
-Bdot_body = ([Bx2;By2;Bz2] - [Bx;By;Bz]) / 0.1;
-  
+    Bdot_body = ([Bx2;By2;Bz2] - [Bx;By;Bz]) / 0.1;
+    
 
-M(3,1)= Kd * Bdot_body(3);
-if (abs(B_body(2))>abs(B_body(1)))
-    M(1,1)= Ks*(abs(w_b_ib(3))-w_ref)*sign(B_body(2));
-    M(2,1)= 0;
-elseif (abs(B_body(2))<abs(B_body(1)))
-    M(1,1)= 0;
-    M(2,1)=-Ks*(abs(w_b_ib(3))-w_ref)*sign(B_body(1));
-end  
+    M(3,1)= Kd * Bdot_body(3);
+    if (abs(B_body(2))>abs(B_body(1)))
+        M(1,1)= Ks*(abs(w_b_ib(3))-w_ref)*sign(B_body(2));
+        M(2,1)= 0;
+    elseif (abs(B_body(2))<abs(B_body(1)))
+        M(1,1)= 0;
+        M(2,1)=-Ks*(abs(w_b_ib(3))-w_ref)*sign(B_body(1));
+    end  
 
-M = mtq_scaling(M, mtq_max);
-T_magnetic_effective = cross(M, B_body);
-torq = T_magnetic_effective;
+    M = mtq_scaling(M, mtq_max);
+    T_magnetic_effective = cross(M, B_body);
+    torq = T_magnetic_effective;
 
-%% Calculate V, I, P of MTQ's
-[V_mtq, I_mtq, P_thermal_mtq] = mtq_model(M);
-       
+    %% Calculate V, I, P of MTQ's
+    [V_mtq, I_mtq, P_thermal_mtq] = mtq_model(M);
+        
 end
    
