be149676fedf83e1710af6365a897e53a3e737ed
Alexandra Tsela <atsela@ece.auth.gr>
2025-02-25 13:52:57 +0000

    add comments to thomson controller

--- src/Z-Thomson/Control_Thomson.m
+++ src/Z-Thomson/Control_Thomson.m
@@ -1,30 +1,53 @@
+% ===============================================================================================
+%  Implementation of the controller that calculates the desired torque
+%  to be applied in order to achieve Z-Thomson
+%
+%  Inputs:
+%   w_b_ib         -Angular velocity of the ECI frame with
+%                   respect to the body frame, expressed in the
+%                   body frame
+%  B_body          -Estimated magnetic field expressed in body frame
+%  B_body_2        -Second magnetometer measurement 
+%  mtq_max         -Maximum dipole provided by each Magnetorquer
+%  Kd              -Gain for Detumbling
+%  Ks              -Gain for Thomson
+%  w_ref           -Desired angular velocity ,around z-axis of body frame with
+%                   respect to the ECI frame , expressed in the body frame 
+%
+%  Outputs:
+%  Bdot_body       -Magnetic field Deviation 
+%  torq            -Applied torque
+%  V_mtq           -Voltage applied on mtq
+%  I_mtq           -Amperage applied on RW
+%  P_thermal_mtq   -Power consumed from RW 
+%  M               -Magnetic dipole
 
 function [Bdot_body,torq,V_mtq, I_mtq, P_thermal_mtq,M] = ...
-        Control_Thomson(w_b_ib, B2,B, mtq_max, Kd, Ks, w_ref)
+        Control_Thomson(w_b_ib, B_body_2,B_body, mtq_max, Kd, Ks, w_ref)
 
-Bx=acos(B(1)/norm(B));
-Bx2=acos(B2(1)/norm(B2));
+Bx=acos(B_body(1)/norm(B_body));
+Bx2=acos(B_body_2(1)/norm(B_body_2));
 
-By=acos(B(2)/norm(B));
-By2=acos(B2(2)/norm(B2));
+By=acos(B_body(2)/norm(B_body));
+By2=acos(B_body_2(2)/norm(B_body_2));
 
-Bz=acos(B(3)/norm(B));
-Bz2=acos(B2(3)/norm(B2));
+Bz=acos(B_body(3)/norm(B_body));
+Bz2=acos(B_body_2(3)/norm(B_body_2));
 
 Bdot_body = ([Bx2;By2;Bz2] - [Bx;By;Bz]) / 0.1;
   
 
 M(3,1)= Kd * Bdot_body(3);
-if (abs(B(2))>abs(B(1)))
-    M(1,1)= Ks*(abs(w_b_ib(3))-w_ref)*sign(B(2));
+if (abs(B_body(2))>abs(B_body(1)))
+    M(1,1)= Ks*(abs(w_b_ib(3))-w_ref)*sign(B_body(2));
     M(2,1)= 0;
-elseif (abs(B(2))<abs(B(1)))
+elseif (abs(B_body(2))<abs(B_body(1)))
     M(1,1)= 0;
-    M(2,1)=-Ks*(abs(w_b_ib(3))-w_ref)*sign(B(1));
+    M(2,1)=-Ks*(abs(w_b_ib(3))-w_ref)*sign(B_body(1));
 end  
 
 M = mtq_scaling(M, mtq_max);
-T_magnetic_effective = cross(M, B);
+T_magnetic_effective = cross(M, B_body);
 torq = T_magnetic_effective;
 
 %% Calculate V, I, P of MTQ's
