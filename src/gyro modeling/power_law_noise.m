263bd122a370d8a0e26b4762f0e129267f402c7c
Alexandra Tsela <atsela@ece.auth.gr>
2025-03-27 17:20:52 +0000

    Applied conjugate symmetry to Fourier components for negative frequencies to ensure a real-valued time series.

--- src/gyro modeling/power_law_noise.m
+++ src/gyro modeling/power_law_noise.m
@@ -1,32 +1,34 @@
 function noise = power_law_noise(beta, v0, dims)
-  
-    n = dims(end);
+   
+    n=dims(end);
     
-    v = (0:floor(n/2)) / n;
-       
-  
-    if ~(0 <= v0 && v0 <= 0.5)
-        error('v0 must be in the range [0, 0.5]');
-    end
-    v0 = max(v0, 1/n);
+    f = (0:floor(n/2))/n;
+    f(1) = 1/n; 
     
-    S = (max(v, v0)).^(-beta / 2);
     
- 
-    X = randn([dims(1:end-1), length(v)]);
-    Y = randn([dims(1:end-1), length(v)]);
+    S = (max(f, v0)).^(-beta/2);
+    
+    
+    X = randn(1,length(f)) .* S;
+    Y = randn(1,length(f)) .* S;
     
    
-    Y(:, 1) = 0; 
-    if mod(n, 2) == 0
-        Y(:, end) = 0; 
+    Y(1) = 0;                   
+    if mod(n,2) == 0
+        Y(end) = 0;            
     end
     
     
-    F = S .* (X + 1i * Y);
+    F = X + 1i*Y;
     
-    noise = real(ifft(F, n, numel(dims)));
-   
+    
+    if mod(n,2) == 0
+        F_full = [F, conj(F(end-1:-1:2))];
+    else
+        F_full = [F, conj(F(end:-1:2))];
+    end
+    
+
+    noise = real(ifft(F_full));
+    noise = noise/std(noise);  
 end
-
-
