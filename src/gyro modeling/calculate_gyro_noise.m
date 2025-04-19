0dfc91c20b61031f6413c9500ff16e57b11d1688
Alexandra Tsela <atsela@ece.auth.gr>
2025-04-19 09:49:45 +0000

    Add scaling factor to adjust units

--- src/gyro modeling/calculate_gyro_noise.m
+++ src/gyro modeling/calculate_gyro_noise.m
@@ -17,11 +17,14 @@
 
 function [White_Noise,Pink_Noise,Red_Noise,total_noise] = calculate_gyro_noise(ARW,RRW,BI,N)
 
-v0 = 0.1;
+v0 = 1e-3;
+dt=0.1 ;%Time interval at which the samples from the characterization were taken
 
-White_Noise = ARW * power_law_noise(0,v0,N);
+
+
+White_Noise = ARW/sqrt(dt) * power_law_noise(0,v0,N);
 Pink_Noise = BI* power_law_noise(1,v0,N);
-Red_Noise = RRW * power_law_noise(2,v0,N);
+Red_Noise = RRW*sqrt(dt) * power_law_noise(2,v0,N);
 
 total_noise= White_Noise + Pink_Noise + Red_Noise;
 
