8ee7fa6000daa94b9c92941e53662a64ae16ba55
Alexandra Tsela <atsela@ece.auth.gr>
2025-04-19 08:27:53 +0000

    Update 2 files
    - /src/gyro modeling/power_law_noise.m
    - /src/gyro modeling/calculate_gyro_noise.m

--- src/gyro modeling/calculate_gyro_noise.m
+++ src/gyro modeling/calculate_gyro_noise.m
@@ -1,3 +1,20 @@
+% This function calculates the total gyro noise and the gyro noise components: White Noise,
+% Pink Noise, and Red Noise, based on the given noise parameters (ARW, RRW, and BI). 
+% The function uses the `power_law_noise` function to generate each noise type 
+% with specific exponents (0 for White, 1 for Pink, and 2 for Red).
+%
+% INPUTS:
+% - ARW: The Angular Random Walk (ARW) parameter, used to scale White Noise((rad/sec)/sqrt(Hz).
+% - RRW: The Random Rate Walk (RRW) parameter, used to scale Red Noise(rad/sec)sqrt(Hz).
+% - BI: The Bias Instability (BI) parameter, used to scale Pink Noise (rad/sec).
+% - N: The dimensions of the output noise signal.
+%
+% OUTPUTS:
+% - White_Noise: The generated White Noise signal, scaled by ARW.
+% - Pink_Noise: The generated Pink Noise signal, scaled by BI.
+% - Red_Noise: The generated Red Noise signal, scaled by RRW.
+% - total_noise: The sum of White, Pink, and Red noise components.
+
 function [White_Noise,Pink_Noise,Red_Noise,total_noise] = calculate_gyro_noise(ARW,RRW,BI,N)
 
 v0 = 0.1;
--- src/gyro modeling/power_law_noise.m
+++ src/gyro modeling/power_law_noise.m
@@ -1,3 +1,47 @@
+% This function generates power law noise with a specified exponent (beta),
+% and scaling parameter(v0) , in a specified dimensional space(dims). The
+% power law scaling is applied in the frequency domain , followed by an
+% inverse FFT to generate the time-domain noise signal . The noise is then
+% normalized based on the scaling parameters . 
+%
+%
+% INPUTS:
+% - beta: The exponent of the power law .
+% - v0: The minimum frequency  in the range [0, 0.5].
+% - dims: A vector defining the dimensions of the generated noise, where 
+%         dims(end) specifies the length of the time-domain signal.
+%
+% OUTPUTS:
+% - noise: The generated power law noise signal in the time domain.
+%
+% AUTHOR:
+% Ujjwal Panda (ujjwalpanda97@gmail.com)
+%
+% LICENSE:
+% MIT License
+%
+% Copyright (c) 2022-2023 Ujjwal Panda <ujjwalpanda97@gmail.com>
+%
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in all
+% copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% SOFTWARE.
+%
+
+
 function noise = power_law_noise(beta, v0, dims)
    
     n=dims(end);
